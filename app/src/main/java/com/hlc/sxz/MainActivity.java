39daaf0f769e6484ab751c21bfcec8a06462737a
lchuang8211 <64480625+lchuang8211@users.noreply.github.com>
2020-12-24 00:29:51 +0800

    init project

---
+++ app/src/main/java/com/hlc/sxz/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hlc.sxz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
